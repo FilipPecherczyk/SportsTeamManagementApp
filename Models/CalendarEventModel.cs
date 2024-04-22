7bc689b3d9f74f12f506fd434aea32cd7420552f
Filip_Pecherczyk <pecherczyk.filip@icloud.com>
2024-04-22 20:13:05 +0200

    Schedule and styles update

---
+++ Models/CalendarEventModel.cs
@@ -0,0 +1,54 @@
+﻿using SportsTeamManagementApp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsTeamManagementApp.Models
+{
+    public class CalendarEventModel : BaseModel
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private DateTime _date;
+        public DateTime Date
+        {
+            get { return _date; }
+            set
+            {
+                if (_date != value)
+                {
+                    _date = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _time;
+        public string Time
+        {
+            get { return _time; }
+            set
+            {
+                if (_time != value)
+                {
+                    _time = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
--- ViewModels/ScheduleViewModel.cs
+++ ViewModels/ScheduleViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using SportsTeamManagementApp.Common;
+using SportsTeamManagementApp.Models;
 using SportsTeamManagementApp.Views;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +13,22 @@ namespace SportsTeamManagementApp.ViewModels
     public class ScheduleViewModel : BaseViewModel
     {
         private readonly ScheduleView View;
+        public ObservableCollection<CalendarEventModel> Events { get; set; }  = new ObservableCollection<CalendarEventModel>();
 
         public ScheduleViewModel(ScheduleView view)
         {
             View = view;
+
+            OnLoad();
+        }
+
+        private void OnLoad()
+        {
+            Events = new ObservableCollection<CalendarEventModel>()
+            {
+                new CalendarEventModel { Name = "Trening", Date = DateTime.Today, Time = "16:00" },
+                new CalendarEventModel { Name = "Odnowa", Date = DateTime.Today, Time = "18:30" }
+            };
         }
     }
 }
