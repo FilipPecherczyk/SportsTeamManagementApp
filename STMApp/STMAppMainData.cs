0b6d3f53c79a97a02ec04796cf9891e42ceb7a4c
Filip_Pecherczyk <pecherczyk.filip@icloud.com>
2024-05-12 13:11:49 +0200

    Home View Update

---
+++ STMApp/STMAppMainData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsTeamManagementApp.STMApp
+{
+    public static class STMAppMainData
+    {
+        //public static string LogedUser { get; set; }
+    }
+}
