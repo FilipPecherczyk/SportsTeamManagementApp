49bdc519203202c1b930948e002d481f13888072
Filip_Pecherczyk <pecherczyk.filip@icloud.com>
2024-06-17 14:10:55 +0200

    Buttons in team side done

--- ViewModels/TeamViewModel.cs
+++ ViewModels/TeamViewModel.cs
@@ -1,4 +1,5 @@
-﻿using SportsTeamManagementApp.Common;
+﻿using Newtonsoft.Json;
+using SportsTeamManagementApp.Common;
 using SportsTeamManagementApp.DbAction;
 using SportsTeamManagementApp.Enums;
 using SportsTeamManagementApp.Extensions;
@@ -10,6 +11,7 @@ using SportsTeamManagementApp.Views;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,10 +23,14 @@ namespace SportsTeamManagementApp.ViewModels
     public class TeamViewModel : BaseViewModel
     {
         private readonly TeamView View;
+        private int DataBaseButtonId;
 
         public TeamViewModel(TeamView view)
         {
             View = view;
+            FirstButton = new TeamButtonModel();
+            SecondButton = new TeamButtonModel();
+            ThirdButton = new TeamButtonModel();
             OnLoad();
         }
 
@@ -36,6 +42,12 @@ namespace SportsTeamManagementApp.ViewModels
             var annoucement = AnnouncementDbAction.GetLatestAnnoucementByTeamId(STMAppMainData.LogedUserTeam.Id);
             if (annoucement != null) Annoucement = annoucement.Text;
 
+            FirstButton.IndexNumber = 1;
+            SecondButton.IndexNumber = 2;
+            ThirdButton.IndexNumber = 3;
+            DataBaseButtonId = 0;
+
+            GetButtonsInfo();
             SetVisibilityAndEnabled();
         }
 
@@ -55,6 +67,34 @@ namespace SportsTeamManagementApp.ViewModels
                 SaveCancelAnnoucementVisiblity = Visibility.Hidden;
                 EditTeamButtonsVisibility = Visibility.Hidden;
             }
+
+            if (DataBaseButtonId != 0)
+            {
+                FirstBtnVisiblity = FirstButton.IsActive ? Visibility.Visible : Visibility.Collapsed;
+                SecondBtnVisiblity = SecondButton.IsActive ? Visibility.Visible : Visibility.Collapsed;
+                ThirdBtnVisiblity = ThirdButton.IsActive ? Visibility.Visible : Visibility.Collapsed;
+            }
+            else
+            {
+                FirstBtnVisiblity = Visibility.Hidden;
+                SecondBtnVisiblity = Visibility.Hidden;
+                ThirdBtnVisiblity = Visibility.Hidden;
+            }
+        }
+
+        private void GetButtonsInfo()
+        {
+            var buttonsDomain = ButtonsDbAction.GetButtons();
+            if (buttonsDomain != null)
+            {
+                var buttonsList = JsonConvert.DeserializeObject<List<TeamButtonModel>>(buttonsDomain.ButtonsJSON);
+
+                FirstButton = buttonsList[0];
+                SecondButton = buttonsList[1];
+                ThirdButton = buttonsList[2];
+
+                DataBaseButtonId = buttonsDomain.Id;
+            }
         }
 
         #region Properties
@@ -68,6 +108,48 @@ namespace SportsTeamManagementApp.ViewModels
                 if (_gridDataSheet != value)
                 {
                     _gridDataSheet = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private TeamButtonModel _firstButton;
+        public TeamButtonModel FirstButton
+        {
+            get { return _firstButton; }
+            set
+            {
+                if (_firstButton != value)
+                {
+                    _firstButton = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private TeamButtonModel _secondButton;
+        public TeamButtonModel SecondButton
+        {
+            get { return _secondButton; }
+            set
+            {
+                if (_secondButton != value)
+                {
+                    _secondButton = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private TeamButtonModel _thirdButton;
+        public TeamButtonModel ThirdButton
+        {
+            get { return _thirdButton; }
+            set
+            {
+                if (_thirdButton != value)
+                {
+                    _thirdButton = value;
                     OnPropertyChanged();
                 }
             }
@@ -140,6 +222,179 @@ namespace SportsTeamManagementApp.ViewModels
                     _editTeamButtonsVisibility = value;
                     OnPropertyChanged();
                 }
+            }
+        }
+
+
+        private Visibility _firstBtnVisiblity;
+        public Visibility FirstBtnVisiblity
+        {
+            get { return _firstBtnVisiblity; }
+            set
+            {
+                if (_firstBtnVisiblity != value)
+                {
+                    _firstBtnVisiblity = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private Visibility _secondBtnVisiblity;
+        public Visibility SecondBtnVisiblity
+        {
+            get { return _secondBtnVisiblity; }
+            set
+            {
+                if (_secondBtnVisiblity != value)
+                {
+                    _secondBtnVisiblity = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private Visibility _thirdBtnVisiblity;
+        public Visibility ThirdBtnVisiblity
+        {
+            get { return _thirdBtnVisiblity; }
+            set
+            {
+                if (_thirdBtnVisiblity != value)
+                {
+                    _thirdBtnVisiblity = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        #endregion
+
+        #region Buttons operations
+
+        public ICommand FirstBtnCommand
+        {
+            get
+            {
+                string commandName = "FirstBtnCommand";
+                if (_relayCommands.TryGetValue(commandName, out RelayCommand command))
+                {
+                    return command;
+                }
+                command = new RelayCommand(param => this.FirstBtnExecute());
+                return _relayCommands[commandName] = command;
+            }
+
+            set { }
+        }
+
+        private async void FirstBtnExecute()
+        {
+            await Task.Run(() =>
+            {
+                FirstBtn();
+            });
+        }
+
+        private void FirstBtn()
+        {
+            var link = FirstButton.Link;
+
+            try
+            {
+                System.Diagnostics.Process.Start(new ProcessStartInfo
+                {
+                    FileName = link,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"Błędny adres strony internetowej");
+            }
+        }
+
+        public ICommand SecondBtnCommand
+        {
+            get
+            {
+                string commandName = "SecondBtnCommand";
+                if (_relayCommands.TryGetValue(commandName, out RelayCommand command))
+                {
+                    return command;
+                }
+                command = new RelayCommand(param => this.SecondBtnExecute());
+                return _relayCommands[commandName] = command;
+            }
+
+            set { }
+        }
+
+        private async void SecondBtnExecute()
+        {
+            await Task.Run(() =>
+            {
+                SecondBtn();
+            });
+        }
+
+        private void SecondBtn()
+        {
+            var link = SecondButton.Link;
+
+            try
+            {
+                System.Diagnostics.Process.Start(new ProcessStartInfo
+                {
+                    FileName = link,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"Błędny adres strony internetowej");
+            }
+
+        }
+
+        public ICommand ThirdBtnCommand
+        {
+            get
+            {
+                string commandName = "ThirdBtnCommand";
+                if (_relayCommands.TryGetValue(commandName, out RelayCommand command))
+                {
+                    return command;
+                }
+                command = new RelayCommand(param => this.ThirdBtnExecute());
+                return _relayCommands[commandName] = command;
+            }
+
+            set { }
+        }
+
+        private async void ThirdBtnExecute()
+        {
+            await Task.Run(() =>
+            {
+                ThirdBtn();
+            });
+        }
+
+        private void ThirdBtn()
+        {
+            var link = ThirdButton.Link;
+
+            try
+            {
+                System.Diagnostics.Process.Start(new ProcessStartInfo
+                {
+                    FileName = link,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"Błędny adres strony internetowej");
             }
         }
 
